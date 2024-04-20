378297f9db90d2bbc317c75a4ed72633fcc088af
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-04-20 09:31:12 -0300

    fix: alter column level in table levels to string

--- database/migrations/20240419233831_create-levels.ts
+++ database/migrations/20240419233831_create-levels.ts
@@ -3,7 +3,7 @@ import type { Knex } from 'knex'
 export async function up (knex: Knex): Promise<void> {
   await knex.schema.createTable('levels', (table) => {
     table.uuid('id').notNullable().primary()
-    table.text('level').notNullable()
+    table.string('level').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
     table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
   })
