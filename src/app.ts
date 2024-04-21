5e40f4fc1db1757a9d9e44ce5b058f691acc9046
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-04-21 20:38:55 -0300

    build: install @fastify/cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
 import { levelsRoutes } from './routes/levels'
 import { developersRoutes } from './routes/developers'
+import cors from '@fastify/cors'
 
 export const app = fastify()
+
+void app.register(cors)
 
 void app.register(levelsRoutes, {
   prefix: '/niveis'
